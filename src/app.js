adbd953490f78b70d6bcf818fb5f6c6bb551babc
Derci Santos <dercilima.si@gmail.com>
2024-07-22 06:46:04 -0300

    Removido os endpoints do arquivo app.js

--- src/app.js
+++ src/app.js
@@ -1,64 +1,18 @@
 import express from "express";
+import connectDatabase from "./config/dbconnect.js";
+import routes from "./routes/index.js"
+
+const database = await connectDatabase();
+
+database.on("error", (err) => {
+    console.error(`Erro de conexão: ${err}`);
+});
+
+database.on("open", () => {
+    console.log("Conexão ao MongoDB bem sucedida!");
+});
 
 const app = express();
-// Middleware: toda requisição será convertida para o formato json
-app.use(express.json());
-
-const users = [
-	{
-		id: 1,
-		nome: "João da Silva",
-	},
-	{
-		id: 2,
-		nome: "Maria Alice",
-	},
-];
-
-function findIndexOfUserBy(id) {
-    return users.findIndex(user => user.id === id);
-}
-
-app.get("/", (req, res) => {
-	res.status(200).send("Curso de Node.js");
-});
-
-app.get("/users", (req, res) => {
-	res.status(200).json(users);
-});
-
-app.get("/users/:id", (req, res) => {
-    let indexOf = findIndexOfUserBy(Number(req.params.id));
-    if (indexOf >= 0) {
-        res.status(200).json(users[indexOf]);
-    } else {
-        res.status(404).send("Usuário não encontrado!");
-    }
-});
-
-app.post("/users", (req, res) => {
-	// Adiciona o usuário no array
-	users.push(req.body);
-	// Devolver a resposta para o cliente
-	res.status(201).send("Usuário inserido com sucesso!");
-});
-
-app.put("/users/:id", (req, res) => {
-	let id = Number(req.params.id);
-
-	let indexOf = users.findIndex((user) => user.id === id);
-	if (indexOf >= 0) {
-		users[indexOf] = req.body;
-		res.status(200).send("Usuário atualizado com sucesso!");
-	} else {
-		res.send(404).send("Usuário não encontrado!");
-	}
-});
-
-app.delete("/users/:id", (req, res) => {
-    let indexOf = findIndexOfUserBy(Number(req.params.id));
-    users.splice(indexOf, 1);
-    res.status(204).end();
-});
+routes(app);
 
 export default app;
