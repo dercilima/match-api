e32c66a32fb3f698762295f636a9aa283b2e3ba6
Derci Santos <dercilima.si@gmail.com>
2024-07-26 09:07:26 -0300

    Movido os logs de conexão com o banco de dados para o arquivo de conexão.

--- src/config/dbconnect.js
+++ src/config/dbconnect.js
@@ -1,8 +1,15 @@
 import mongoose from "mongoose";
 
 async function connectDatabase() {
-    mongoose.connect(process.env.DB_CONNECTION_STRING);
-    return mongoose.connection;
+	mongoose.connect(process.env.DB_CONNECTION_STRING);
+	
+    mongoose.connection.on("error", (err) => {
+		console.error(`Erro de conexão: ${err}`);
+	});
+
+	mongoose.connection.on("open", () => {
+		console.log("Conexão ao MongoDB bem sucedida!");
+	});
 }
 
 export default connectDatabase;
