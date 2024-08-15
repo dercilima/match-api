80ea432aeb2ea2316eb07edd9bccc6b78368bfbb
Derci Santos <dercilima.si@gmail.com>
2024-08-15 14:39:17 -0300

    Inicialização do Firebase Admin SDK

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import {initializeApp} from "firebase-admin/app";
 import app from "./app";
 
-const PORT = process.env.PORT;
+initializeApp();
 
+const PORT = process.env.PORT ?? 3000;
 app.listen(PORT, () => {
 	console.log(`Servidor escutando na porta ${PORT} ...`);
 });
