03643827cefac292bbcb319bf69fa7274a0975a7
Derci Santos <dercilima.si@gmail.com>
2024-09-06 15:45:55 -0300

    Liberar a rota /auth/login da autenticação

--- functions/src/middlewares/auth.middleware.ts
+++ functions/src/middlewares/auth.middleware.ts
@@ -7,6 +7,10 @@ import { ForbiddenError } from "../errors/forbidden.error";
 
 export const auth = (app: express.Express) => {
 	app.use(async (req: Request, res: Response, next: NextFunction) => {
+		if (req.method === "POST" && req.url.endsWith("/auth/login")) {
+			return next();
+		}
+
 		const token = req.headers.authorization?.split("Bearer ")[1];
 
 		if (token) {
